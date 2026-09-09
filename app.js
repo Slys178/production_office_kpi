023d9c0ace96885b9447fbb667d9ce9e4120d472
Slys178 <simon.ask29@hotmail.com>
2026-09-09 14:49:44 +0100

    Modify USER_LIST_SHEET_ID format
    Updated USER_LIST_SHEET_ID to remove the URL prefix.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
    ========================================================= */
 
 // ===== ADD THIS LINE =====
-const USER_LIST_SHEET_ID = 'https://docs.google.com/spreadsheets/d/1uX_f3jc123mX2SsK7vIVIxN_zpQ3GkUqmA9U0QdJ2Dw/edit?usp=sharing'; // <-- REPLACE THIS!
+const USER_LIST_SHEET_ID = '1uX_f3jc123mX2SsK7vIVIxN_zpQ3GkUqmA9U0QdJ2Dw/edit?usp=sharing'; // <-- REPLACE THIS!
 const USER_LIST_GID = '0';
 
 // Check if user is logged in
